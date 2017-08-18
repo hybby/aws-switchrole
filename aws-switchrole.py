4db282ea0ca9091c04dafd3f559ffc4bc0a5398c
Drew Jess <drew.jess@fanduel.com>
2017-08-18 14:04:57 +0100

    pep8 compliant.  also printinfo

--- aws-switchrole.py
+++ aws-switchrole.py
@@ -15,165 +15,175 @@ from distutils.spawn import find_executable
 
 # colors
 class color:
-  blue = '\033[94m'
-  green = '\033[92m'
-  yellow = '\033[93m'
-  red = '\033[91m'
-  bold = '\033[1m'
-  underline = '\033[4m'
-  normal = '\033[0m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    normal = '\033[0m'
 
 
 # print color codes that correspond with the above class
 def print_color(color):
-  sys.stdout.write(color)  # no pesky newlines plz
-  sys.stdout.flush()
+    sys.stdout.write(color)  # no pesky newlines plz
+    sys.stdout.flush()
 
 
 # red text and bomb out
 def print_error(text):
-  print_color(color.red)
-  print text
-  print_color(color.normal)
-  sys.exit(1)
+    print_color(color.red)
+    print text
+    print_color(color.normal)
+    sys.exit(1)
 
 
 # yellow text
 def print_warning(text):
-  print_color(color.yellow)
-  print text
-  print_color(color.normal)
+    print_color(color.yellow)
+    print text
+    print_color(color.normal)
 
 
 # green text
 def print_ok(text):
-  print_color(color.green)
-  print text
-  print_color(color.normal)
+    print_color(color.green)
+    print text
+    print_color(color.normal)
+
+
+# blue text
+def print_info(text):
+    print_color(color.blue)
+    print text
+    print_color(color.normal)
 
 
 # given a list of config sections, return ones that look like profiles
 def get_profiles(config_sections):
-  profiles = []
-  profile_pattern = re.compile('^profile (\w+)$')
+    profiles = []
+    profile_pattern = re.compile('^profile (\w+)$')
 
-  for section in config_sections:
-    result = profile_pattern.search(section)
-    if result:
-      profiles.append(result.group(1))
+    for section in config_sections:
+        result = profile_pattern.search(section)
+        if result:
+            profiles.append(result.group(1))
 
-  return sorted(profiles)
+    return sorted(profiles)
 
 
 # given a list of profiles, ask the user to pick one and return that.
 def get_profile_choice(profiles):
-  i = 0
-  valid_choice = False
+    i = 0
+    valid_choice = False
 
-  print_warning('no profile provided. please choose your profile:')
+    print_warning('no profile provided. please choose your profile:')
 
-  for profile in profiles:
-    print "  {}. {}".format((i + 1), profile)
-    i += 1
-
-  while not valid_choice:
-    try:
-      choice = int(raw_input("-> "))
-    except ValueError:
-      choice = 0
-
-    if choice > 0 and choice < (len(profiles) + 1):
-        valid_choice = True
-        return profiles[(choice - 1)]
+    if profiles:
+        for profile in profiles:
+            print "  {}. {}".format((i + 1), profile)
+            i += 1
     else:
-      print_warning('please choose a valid value')
-  else:
-    print_error(
-      "FATAL: couldn't find any profiles in '{}'".format(config_file)
-    )
-    sys.exit(1)
+        print_error(
+            "FATAL: couldn't find any profiles in '{}'".format(config_file)
+        )
+
+    while not valid_choice:
+        try:
+            choice = int(raw_input("-> "))
+        except ValueError:
+            choice = 0
+
+        if choice > 0 and choice < (len(profiles) + 1):
+            valid_choice = True
+            return profiles[(choice - 1)]
+        else:
+            print_warning('please choose a valid value')
 
 
 if __name__ == "__main__":
-  # open our aws config file
-  config_file = '~/.aws/config'
-  config = ConfigParser.RawConfigParser()
-  config.read(os.path.expanduser(config_file))
-  profiles = get_profiles(config.sections())
+    # open our aws config file
+    config_file = '~/.aws/config'
+    config = ConfigParser.RawConfigParser()
+    config.read(os.path.expanduser(config_file))
+    profiles = get_profiles(config.sections())
 
-  # parse command-line arguments
-  parser = argparse.ArgumentParser(
-    description='gets temporary credentials for switching to an aws role'
-  )
-
-  parser.add_argument(
-    '--profile',
-    type=str,
-    required=False,
-    help='profile in ~/.aws/config with role you want to switch to'
-  )
-
-  args = parser.parse_args()
-
-  if args.profile:
-    profile = args.profile
-  else:
-    profile = get_profile_choice(profiles)
-
-  print_ok("using profile '{}'".format(profile))
-
-  try:
-    role = config.get("profile {}".format(profile), "role_arn")
-  except:
-    print_error(
-      "FATAL: couldn't find profile '{}' in '{}'".format(profile, config_file)
+    # parse command-line arguments
+    parser = argparse.ArgumentParser(
+        description='gets temporary credentials for switching to an aws role'
     )
 
-  # give our role switch session a name and build our aws command
-  session = profile + '-' + time.strftime('%d%m%y%H%M%S')
-  cmd = [
-    find_executable('aws'), 'sts', 'assume-role',
-    '--role-arn', role,              # Role ARN
-    '--role-session-name', session,  # Session ID given to temp credentials
-    '--profile', profile,            # Profile name from ~/.aws/config
-  ]
+    parser.add_argument(
+        '--profile',
+        type=str,
+        required=False,
+        help='profile in ~/.aws/config with role you want to switch to'
+    )
 
-  process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-  out, err = process.communicate()
+    args = parser.parse_args()
 
-  # load the json response into a dict
-  try:
-    creds = json.loads(out)
-  except:
-    print_warning('problem parsing AWS response. STDOUT and STDERR below:')
-    print_color(color.yellow)
-    print 'STDOUT:'
-    print_color(color.normal)
-    print out
-    print_color(color.yellow)
-    print 'STDERR:'
-    print_color(color.normal)
-    print err
-    sys.exit(1)
+    if args.profile:
+        profile = args.profile
+    else:
+        profile = get_profile_choice(profiles)
 
-  # print out our returned values as export commands, ready to go.
-  try:
-    env_vars = {
-      'AWS_ACCESS_KEY_ID': creds['Credentials']['AccessKeyId'],
-      'AWS_SECRET_ACCESS_KEY': creds['Credentials']['SecretAccessKey'],
-      'AWS_SESSION_TOKEN': creds['Credentials']['SessionToken'],
-      'AWS_SECURITY_TOKEN': creds['Credentials']['SessionToken'],
-    }
+    print_ok("using profile '{}'".format(profile))
 
-    print_ok("got temporary credentials for profile '{}'".format(profile))
-    print "----- load vars below into your env to assume profile's role -----"
+    try:
+        role = config.get("profile {}".format(profile), "role_arn")
+    except:
+        print_error(
+            "FATAL: couldn't find profile '{}' in '{}'".format(
+                profile,
+                config_file
+            )
+        )
 
-    for k, v in env_vars.iteritems():
-      print " export {}={}".format(k, v)
+    # give our role switch session a name and build our aws command
+    session = profile + '-' + time.strftime('%d%m%y%H%M%S')
+    cmd = [
+        find_executable('aws'), 'sts', 'assume-role',
+        '--role-arn', role,              # Role ARN
+        '--role-session-name', session,  # Session ID given to temp credentials
+        '--profile', profile,            # Profile name from ~/.aws/config
+    ]
 
-    print '------------------------------------------------------------------'
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = process.communicate()
 
-  except:
-    print_error('AWS response did not contain expected values. dumping below:')
-    print creds
-    sys.exit(1)
+    # load the json response into a dict
+    try:
+        creds = json.loads(out)
+    except:
+        print_warning('problem parsing AWS response. STDOUT and STDERR below:')
+        print_warning('STDOUT:')
+        print out
+        print_warning('STDERR:')
+        print err
+        sys.exit(1)
+
+    # print out our returned values as export commands, ready to go.
+    try:
+        env_vars = {
+            'AWS_ACCESS_KEY_ID': creds['Credentials']['AccessKeyId'],
+            'AWS_SECRET_ACCESS_KEY': creds['Credentials']['SecretAccessKey'],
+            'AWS_SESSION_TOKEN': creds['Credentials']['SessionToken'],
+            'AWS_SECURITY_TOKEN': creds['Credentials']['SessionToken'],
+        }
+
+        print_ok("got temporary credentials for profile '{}'".format(profile))
+        print_info(
+            "---- load these vars into your env to assume profile's role ----"
+        )
+
+        for k, v in env_vars.iteritems():
+            print " export {}={}".format(k, v)
+
+        print_info(
+            '----------------------------------------------------------------'
+        )
+
+    except:
+        print_error('AWS response did not contain expected valuess. dumping:')
+        print creds
+        sys.exit(1)
