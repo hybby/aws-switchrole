7f26e41c01731f1a84a951922508b0e6f3635007
Drew Jess <drew.jess@fanduel.com>
2018-06-15 16:35:16 +0100

    fix entrypoints for pip package

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name = 'aws-switchrole',
     packages = find_packages(),
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'a tool to allow you to switch AWS roles on your console',
     author = 'hybby',
     author_email = 'iamdrew@gmail.com',
     url = 'https://github.com/hybby/aws-switchrole',
-    download_url = 'https://github.com/hybby/aws-switchrole/archive/0.0.1.tar.gz',
+    download_url = 'https://github.com/hybby/aws-switchrole/archive/0.0.2.tar.gz',
     keywords = ['aws', 'iam', 'sts'],
     install_requires = [
         'awscli>=1.15.25',
         'pyperclip>=1.6.0',
     ],
+    entry_points={
+        'console_scripts': [ 'aws-switchrole=aws_switchrole.aws_switchrole:main' ]
+    },
     classifiers = [],
 )
